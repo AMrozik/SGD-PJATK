90ea089bf49aabc6eca5fcf22e346016e8efae51
Andrzej Mrozik <andrzej.mrozik98@gmail.com>
2021-05-14 12:09:45 +0200

    repo clean

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,9 @@
 #include <tuple>
 #include <vector>
 
+#define HEIGHT 640
+#define WIDTH 480
+
 // check for errors
 #define errcheck(e)                                                            \
   {                                                                            \
@@ -29,19 +32,16 @@
 int main(int, char **) {
   using namespace std;
   using namespace std::chrono;
-  const int width = 640;
-  const int height = 480;
 
   errcheck(SDL_Init(SDL_INIT_VIDEO) != 0);
 
-  shared_ptr<SDL_Window> window(SDL_CreateWindow("My Next Superawesome Game",
+  shared_ptr<SDL_Window> window(SDL_CreateWindow("Pong SDL2 LEEEETS GOOOOO",
                                                  SDL_WINDOWPOS_UNDEFINED,
-                                                 SDL_WINDOWPOS_UNDEFINED, width,
-                                                 height, SDL_WINDOW_SHOWN),
-                                [](SDL_Window *p) {
-                                  cout << "destroyng window";
-                                  SDL_DestroyWindow(p);
-                                });
+                                                 SDL_WINDOWPOS_UNDEFINED,
+                                                 HEIGHT,
+                                                 WIDTH,
+                                                 SDL_WINDOW_SHOWN),
+                                [](SDL_Window *p) { SDL_DestroyWindow(p); });
   errcheck(window == nullptr);
 
   shared_ptr<SDL_Renderer> renderer(
@@ -56,7 +56,8 @@ int main(int, char **) {
 
   steady_clock::time_point current_time =
       steady_clock::now(); // remember current time
-    double x = 0, y = 240;
+      int x=0,y=0;
+      bool point_active = false;
   for (bool game_active = true; game_active;) {
     SDL_Event event;
     while (SDL_PollEvent(&event)) { // check if there are some events
@@ -64,21 +65,54 @@ int main(int, char **) {
       case SDL_QUIT:
         game_active = false;
         break;
-      case SDL_KEYUP:
-        if (event.key.keysym.scancode == SDL_SCANCODE_Q) {
-          game_active = false;
-        }
-        break;
+        case SDL_KEYDOWN:
+          if (event.key.keysym.sym == SDLK_ESCAPE) game_active = false;
+          if(event.key.keysym.sym == SDLK_RIGHT && point_active){
+            x+=1;
+            SDL_SetRenderDrawColor(renderer.get(), 0, 0, 0, 0);
+            SDL_RenderClear(renderer.get());
+            SDL_SetRenderDrawColor(renderer.get(), 255, 255, 255, 255);
+            SDL_RenderDrawPoint(renderer.get(), x, y);
+          }
+          if(event.key.keysym.sym == SDLK_LEFT && point_active){
+            x-=1;
+            SDL_SetRenderDrawColor(renderer.get(), 0, 0, 0, 0);
+            SDL_RenderClear(renderer.get());
+            SDL_SetRenderDrawColor(renderer.get(), 255, 255, 255, 255);
+            SDL_RenderDrawPoint(renderer.get(), x, y);
+          }
+          if(event.key.keysym.sym == SDLK_UP && point_active){
+            y-=1;
+            SDL_SetRenderDrawColor(renderer.get(), 0, 0, 0, 0);
+            SDL_RenderClear(renderer.get());
+            SDL_SetRenderDrawColor(renderer.get(), 255, 255, 255, 255);
+            SDL_RenderDrawPoint(renderer.get(), x, y);
+          }
+          if(event.key.keysym.sym == SDLK_DOWN && point_active){
+            y+=1;
+            SDL_SetRenderDrawColor(renderer.get(), 0, 0, 0, 0);
+            SDL_RenderClear(renderer.get());
+            SDL_SetRenderDrawColor(renderer.get(), 255, 255, 255, 255);
+            SDL_RenderDrawPoint(renderer.get(), x, y);
+          }
+          break;
+        case SDL_MOUSEBUTTONDOWN:
+          SDL_GetMouseState(&x, &y);
+          SDL_SetRenderDrawColor(renderer.get(), 255, 255, 255, 255);
+          SDL_RenderDrawPoint(renderer.get(), x, y);
+          point_active = true;
+          break;
       }
     }
 
-    double px = x, py = y;
-
-    x+=0.6;
-    y = sin(0.1*x/M_PI)*200+240;
-
-    SDL_SetRenderDrawColor(renderer.get(), 255, 0, 0, 255);
-    SDL_RenderDrawLineF(renderer.get(), x, y, px, py);
+    // double x = 0, y = 240;
+    // double px = x, py = y;
+    //
+    // x += 0.6;
+    // y = sin(0.1 * x / M_PI) * 200 + 240;
+    //
+    // SDL_SetRenderDrawColor(renderer.get(), 255, 0, 0, 255);
+    // SDL_RenderDrawLineF(renderer.get(), x, y, px, py);
 
     SDL_RenderPresent(renderer.get()); // draw frame to screen
 
